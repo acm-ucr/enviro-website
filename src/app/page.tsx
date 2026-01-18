4a502d87e2aa0f6ba747ea04980e2dfd8cc7de68
Colin <colintruong168@gmail.com>
2026-01-18 00:09:50 -0800

    Added squiggly border

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
-import Button from "@/components/Button";
 const Home = () => {
   return (
-    <>
-      <div className="flex h-screen w-screen items-center justify-center">
-        Hello World
-        <Button />
-      </div>
-    </>
+    <div className="flex h-screen w-screen items-center justify-center">
+      Hello World
+    </div>
   );
 };
 
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,13 @@
+import Image from "next/image";
+import Border from "@/public/SquigglyBorder.svg";
 const Button = () => {
   return (
     <div className="bg-enviro-green-100 w-1/10 px-2 py-2">
-      <div className="border-2 py-2">
-        <div className="text-enviro-green-300 text-center">Learn More</div>
+      <div className="relative">
+        <Image src={Border} alt="" className="h-auto w-full rounded-xs" />
+        <div className="absolute inset-0 flex items-center justify-center">
+          <p className="text-enviro-green-300">Button</p>
+        </div>
       </div>
     </div>
   );
