20c08f413442d0ea26c49741a024cbc268a197a7
brysach <bryan.estradach@gmail.com>
2026-01-23 19:50:13 -0800

    Finished navbar #7

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,38 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import enviroLogo from "@/public/logo.webp";
+import navigations from "@/data/NavBarLinks";
+import { usePathname } from "next/navigation";
+
 const Navbar = () => {
-  return <div></div>;
+  const pathName = usePathname();
+
+  return (
+    <div className="bg-enviro-blue-100 font-enviro-single-day flex h-36 items-center justify-between text-black">
+      <div className="ml-4">
+        <Image
+          src={enviroLogo}
+          alt="Enviromental Science Club Logo"
+          width={150}
+        />
+      </div>
+
+      <div className="mr-10 flex items-center justify-center gap-16 text-4xl">
+        {navigations.map(({ link, name }, index) => (
+          <div key={index}>
+            <Link
+              href={link}
+              className={pathName === link ? "text-white" : "text-black"}
+            >
+              {name}
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
---
+++ src/data/NavBarLinks.ts
@@ -0,0 +1,27 @@
+export interface navigation {
+  name: string;
+  link: string;
+}
+const navigations: navigation[] = [
+  {
+    name: "Home",
+    link: "/home",
+  },
+  {
+    name: "About",
+    link: "/link",
+  },
+  {
+    name: "Board",
+    link: "/board",
+  },
+  {
+    name: "Events",
+    link: "/events",
+  },
+  {
+    name: "Contact Us",
+    link: "/contact",
+  },
+];
+export default navigations;
