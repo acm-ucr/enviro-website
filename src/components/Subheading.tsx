770fae53c042d24c364ee38a5eb4088866bb21fa
Howardwheeler <howard.wheeler159@gmail.com>
2026-01-26 19:20:46 -0800

    format

--- src/components/Subheading.tsx
+++ src/components/Subheading.tsx
@@ -6,7 +6,9 @@ interface SubheadingProps {
 
 const Subheading = ({ title, bg, text }: SubheadingProps) => {
   return (
-    <div className={`${bg} border-enviro-green-300 w-1/2 rounded-4xl border-1 py-4`}>
+    <div
+      className={`${bg} border-enviro-green-300 w-1/2 rounded-4xl border-1 py-4`}
+    >
       <div className={`font-enviro-single-day text-center text-5xl ${text}`}>
         {title}
       </div>
