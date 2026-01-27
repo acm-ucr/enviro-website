a3d84b0c70cddd8de9075c25e88c6d35b0e23f3a
Howardwheeler <howard.wheeler159@gmail.com>
2026-01-26 19:20:15 -0800

    subheading is good // added colors and subheading reference

--- src/components/Subheading.tsx
+++ src/components/Subheading.tsx
@@ -1,11 +1,13 @@
 interface SubheadingProps {
   title: string;
+  bg: string;
+  text: string;
 }
 
-const Subheading = ({ title }: SubheadingProps) => {
+const Subheading = ({ title, bg, text }: SubheadingProps) => {
   return (
-    <div className="bg-enviro-green-200 border-enviro-green-300 w-1/2 rounded-4xl border-1 py-4">
-      <div className="font-enviro-single-day text-center text-3xl text-white">
+    <div className={`${bg} border-enviro-green-300 w-1/2 rounded-4xl border-1 py-4`}>
+      <div className={`font-enviro-single-day text-center text-5xl ${text}`}>
         {title}
       </div>
     </div>
