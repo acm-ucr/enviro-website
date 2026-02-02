9068cd8dda6f08eee45acb85027e7c669ecef1db
Stanley Xiao <151390143+stanleyx027@users.noreply.github.com>
2026-02-02 01:41:02 -0800

    Refactor OurMission component layout and styles

--- src/components/about/OurMission.tsx
+++ src/components/about/OurMission.tsx
@@ -3,50 +3,42 @@ import Image from "next/image";
 import MissionStatement from "@/public/Home/MissionStatement.webp";
 import Tape from "@/public/tape.webp";
 import Flower from "@/public/Flower.webp";
-import Gradient from "@/public/Gradient.webp";
 import Line from "@/public/Home/SquigglyLine.svg";
 
 const OurMission = () => {
   return (
-    <div className="relative h-[600px] w-screen">
-      {/* Background */}
-      <Image
-        src={Gradient}
-        alt=""
-        className="absolute inset-0 h-full w-full object-cover"
-      />
-
+    <div className="h-full w-full bg-[url(/Home/Gradient.webp)]">
       {/* Content */}
-      <div className="relative z-10 flex h-full items-center justify-around">
+      <div className="z-10 flex h-full items-center justify-around">
         {/* Left side */}
         <div className="relative h-8/12 w-5/12">
-          <div className="bg-enviro-gray-100 h-full w-full">
+          <div className="bg-enviro-gray-100 h-full w-full shadow-lg">
             <Image
               src={MissionStatement}
               alt=""
-              className="border-enviro-green-400 h-9/10 w-full scale-[0.9] border object-cover"
+              className="relative top-1 border-enviro-green-400 h-9/10 scale-[0.93] border object-cover"
             />
           </div>
           {/* Tape */}
           <Image
             src={Tape}
             alt=""
-            className="absolute -top-4 left-1/2 w-80 -translate-x-1/2 opacity-50"
+            className="absolute -top-5 left-1/2 w-80 -translate-x-1/2 opacity-70"
           />
           {/* Flower */}
           <Image
             src={Flower}
             alt=""
-            className="absolute -bottom-72 left-0.5 -translate-x-1/2 scale-[0.8]"
+            className="absolute -bottom-70 left-0.5 -translate-x-1/2 scale-[0.8]"
           />
         </div>
         {/* Right side */}
-        <div className="relative flex h-96 w-4/10 flex-col items-center justify-between text-center text-white">
-          <div className="font-enviro-single-day text-3xl">
+        <div className="flex h-8/12 w-4/10 flex-col items-center gap-2 text-center text-white">
+          <div className="font-enviro-single-day text-6xl">
             Our Mission Statement
           </div>
-          <Image src={Line} alt="" className="h-auto w-1/2 -translate-y-6" />
-          <div className="w-1/2 -translate-y-4 text-lg">
+          <Image src={Line} alt="Squiggly Line" className="w-full pb-5"/>
+          <div className="text-3xl pb-10">
             We are the Environmental Science Club, a vibrant community of
             students committed to understanding and protecting the environment.
             Our mission is to raise global awareness about ecological issues and
