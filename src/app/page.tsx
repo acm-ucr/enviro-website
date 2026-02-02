c31d2c1201d5a5b41af4ee720e7a886e6b408354
Stanley Xiao <151390143+stanleyx027@users.noreply.github.com>
2026-02-02 01:39:32 -0800

    Remove OurMission component from Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import OurMission from "@/components/about/OurMission";
-
 const Home = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <OurMission />
+      Home
     </div>
   );
 };
