3e351d479974a17457fc09e7e21e450254a6fc0e
Draylend <draylend0821@gmail.com>
2026-02-07 23:40:21 -0800

    Add upcoming event component
    - Edited src/app/events/page.tsx to call UpcomingEvents component
    - Edited src/app/page.tsx to call UpcomingEvents component
    - Edited src/components/EventsCard.tsx to fix colors
    - Added UpcomingEvents.tsx to src/components/ for UpcomingEvents component
        - I put this component in the general folder because it's called in both /Home and /Events

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import UpcomingEvents from "@/components/UpcomingEvents";
+
 const Home = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      Home
+    <div className="">
+      <UpcomingEvents />
     </div>
   );
 };
