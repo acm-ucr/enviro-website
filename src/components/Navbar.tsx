6989ef70f1901724396d2834bc493c4d1e1af9c4
brysach <bryan.estradach@gmail.com>
2026-03-09 13:33:46 -0700

    Made navbar mobile responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ import navbarClose from "@/public/navbarMobile.webp";
 import navbarOpen from "@/public/navbarMobileOpen.webp";
 import navbarLine from "@/public/navbarLine.webp";
 import navigations from "@/data/NavBarLinks";
-import {useState} from "react";
+import { useState } from "react";
 
 import { usePathname } from "next/navigation";
 
@@ -18,117 +18,112 @@ const Navbar = () => {
 
   return (
     <div className="font-enviro-single-day fixed top-0 left-0 z-50 w-screen text-black">
-<div className="relative w-full md:hidden">
-{isOpen ? (
-  <div className="relative w-full h-[300px]">
-    <Image
-      src={navbarOpen}
-      alt="Navbar background open"
-      fill
-      sizes="100vw"
-      className="object-cover"
-    />
-  </div>
-) : (
-  <div className="relative w-full h-[71px]">
-    <Image
-      src={navbarClose}
-      alt="Navbar background closed"
-      fill
-      sizes="100vw"
-      className="object-cover"
-    />
-  </div>
-)}
+      <div className="relative w-full md:hidden">
+        {isOpen ? (
+          <div className="relative h-[300px] w-full">
+            <Image
+              src={navbarOpen}
+              alt="Navbar background open"
+              fill
+              sizes="100vw"
+              className="object-cover"
+            />
+          </div>
+        ) : (
+          <div className="relative h-[71px] w-full">
+            <Image
+              src={navbarClose}
+              alt="Navbar background closed"
+              fill
+              sizes="100vw"
+              className="object-cover"
+            />
+          </div>
+        )}
 
-  <div className="absolute inset-0 z-10">
-    <Link href="/" className="absolute left-4 items-center">
-      <Image
-        src={enviroLogo}
-        alt="Environmental Science Club Logo"
-        width={70}
-        height={70}
-        className="shrink-0"
-      />
-    </Link>
+        <div className="absolute inset-0 z-10">
+          <Link href="/" className="absolute left-4 items-center">
+            <Image
+              src={enviroLogo}
+              alt="Environmental Science Club Logo"
+              width={70}
+              height={70}
+              className="shrink-0"
+            />
+          </Link>
 
-    <button
-      type="button"
-      onClick={() => setIsOpen(!isOpen)}
-      className="absolute right-4 top-4 flex flex-col gap-2 p-2"
-    >
-      <span className="block h-0.5 w-7 bg-black"></span>
-      <span className="block h-0.5 w-7 bg-black"></span>
-      <span className="block h-0.5 w-7 bg-black"></span>
-    </button>
+          <button
+            type="button"
+            onClick={() => setIsOpen(!isOpen)}
+            className="absolute top-4 right-4 flex flex-col gap-2 p-2"
+          >
+            <span className="block h-0.5 w-7 bg-black"></span>
+            <span className="block h-0.5 w-7 bg-black"></span>
+            <span className="block h-0.5 w-7 bg-black"></span>
+          </button>
 
-    {isOpen && (
-      <div className="pt-20">
-        <div className="flex justify-center px-3">
-          <Image
-            src={navbarLine}
-            alt=""
-            className="w-96 h-[1.5px]"
-          />
-        </div>
+          {isOpen && (
+            <div className="pt-20">
+              <div className="flex justify-center px-3">
+                <Image src={navbarLine} alt="" className="h-[1.5px] w-96" />
+              </div>
 
-        <div className="mt-3 flex flex-col items-center gap-2 text-2xl">
-          {navigations.map(({ link, name }, index) => (
-            <Link
-              key={index}
-              href={link}
-              onClick={() => setIsOpen(false)}
-              className={[
-                "text-center transition rounded-full",
-                pathName === link
-                  ? "bg-enviro-green-100 px-3 text-white"
-                  : "text-black",
-              ].join(" ")}
-            >
-              {name}
-            </Link>
-          ))}
+              <div className="mt-3 flex flex-col items-center gap-2 text-2xl">
+                {navigations.map(({ link, name }, index) => (
+                  <Link
+                    key={index}
+                    href={link}
+                    onClick={() => setIsOpen(false)}
+                    className={[
+                      "rounded-full text-center transition",
+                      pathName === link
+                        ? "bg-enviro-green-100 px-3 text-white"
+                        : "text-black",
+                    ].join(" ")}
+                  >
+                    {name}
+                  </Link>
+                ))}
+              </div>
+            </div>
+          )}
         </div>
       </div>
-    )}
-  </div>
-</div>
 
-      
-      <div className = "relative hidden h-45 w-full md:block">
-      <Image
-        className="overflow-hidden object-cover object-top"
-        src={navbarBg}
-        alt="NavBar Background"
-        fill
-      />
-
-      <div className="absolute inset-0 z-10 flex px-6">
+      <div className="relative hidden h-45 w-full md:block">
         <Image
-          className="-ml-5 shrink-0 scale-90"
-          src={enviroLogo}
-          alt="Environmental Science Club Logo"
-          width={180}
+          className="overflow-hidden object-cover object-top"
+          src={navbarBg}
+          alt="NavBar Background"
+          fill
         />
 
-        <div className="ml-auto flex items-center justify-end gap-15 text-4xl">
-          {navigations.map(({ link, name }, index) => (
-            <div key={index}>
-              <Link
-                href={link}
-                className={[
-                  "flex items-center rounded-full transition",
-                  pathName === link
-                    ? "bg-enviro-green-100 px-6 text-white"
-                    : "text-black",
-                ].join(" ")}
-              >
-                {name}
-              </Link>
-            </div>
-          ))}
+        <div className="absolute inset-0 z-10 flex px-6">
+          <Image
+            className="-ml-5 shrink-0 scale-90"
+            src={enviroLogo}
+            alt="Environmental Science Club Logo"
+            width={180}
+          />
+
+          <div className="ml-auto flex items-center justify-end gap-15 text-4xl">
+            {navigations.map(({ link, name }, index) => (
+              <div key={index}>
+                <Link
+                  href={link}
+                  className={[
+                    "flex items-center rounded-full transition",
+                    pathName === link
+                      ? "bg-enviro-green-100 px-6 text-white"
+                      : "text-black",
+                  ].join(" ")}
+                >
+                  {name}
+                </Link>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
       </div>
     </div>
   );
