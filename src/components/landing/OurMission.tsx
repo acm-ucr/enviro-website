3a4f078aea489ab0b423c861c46477cabba251d6
stanleyx027 <sxiao027@ucr.edu>
2026-03-24 23:47:07 -0700

    Add and adjust some mobile responsiveness to OurMission and Title

--- src/components/landing/OurMission.tsx
+++ src/components/landing/OurMission.tsx
@@ -10,16 +10,16 @@ import Flower from "@/public/Flower.webp";
 
 const OurMission = () => {
   return (
-    <div className="grid">
+    <div className="relative grid">
       <Image
         src={Gradient}
         alt="Gradient Background"
-        className="z-0 col-start-1 row-start-1 h-10/12 w-full"
+        className="z-0 col-start-1 row-start-1 h-10/12 w-full sm:-mt-10 2xl:w-screen"
       />
       {/* Content */}
-      <div className="z-10 col-start-1 row-start-1 flex flex-col items-center justify-around md:-mt-30 md:flex-row">
+      <div className="z-10 col-start-1 row-start-1 flex flex-col items-center justify-around md:absolute md:top-[15vh] md:flex-row">
         {/* Left side */}
-        <div className="relative mt-15 w-full md:-mt-50 md:w-1/2">
+        <div className="relative mt-15 w-full md:-top-[15vh] md:w-1/2">
           <div className="bg-enviro-gray-100 mx-5 pb-10 shadow-lg">
             <Image
               src={MissionStatement}
@@ -31,28 +31,28 @@ const OurMission = () => {
           <Image
             src={Tape}
             alt=""
-            className="absolute -top-5 left-1/2 w-50 -translate-x-1/2 opacity-80 md:w-80"
+            className="absolute -top-5 left-1/2 w-50 -translate-x-1/2 opacity-80 md:w-55"
           />
         </div>
         {/* Right side */}
         <div className="flex h-8/12 w-full flex-col items-center gap-2 text-center text-white md:w-4/10">
-          <div className="font-enviro-single-day mt-2 text-3xl md:mt-0 md:text-4xl lg:text-5xl xl:text-6xl">
+          <div className="font-enviro-single-day mt-2 text-3xl sm:text-4xl md:mt-0 md:text-4xl md:whitespace-nowrap lg:text-5xl xl:text-6xl">
             Our Mission Statement
           </div>
           <Image
             src={Line}
             alt="Squiggly Line"
-            className="pointer-events-none w-9/12 pb-5 md:w-full"
+            className="pointer-events-none w-9/12 pb-5 sm:pb-2 md:w-full"
           />
-          <div className="text-md w-10/12 pb-10 lg:text-xl xl:text-3xl">
+          <div className="text-md w-10/12 pb-5 sm:pb-2 sm:text-lg md:w-full lg:text-xl xl:text-2xl">
             We are the Environmental Science Club, a vibrant community of
             students committed to understanding and protecting the environment.
             Our mission is to raise global awareness about ecological issues and
             inspire action through education, advocacy, and hands-on
             involvement.
           </div>
-          <div className="w-1/2">
-            <Button href="" textSize="text-xl lg:text-2xl xl:text-3xl">
+          <div className="w-5/12 sm:w-1/3 md:w-1/2 xl:w-5/12">
+            <Button href="" textSize="text-lg lg:text-2xl xl:text-2xl">
               Learn More
             </Button>
           </div>
@@ -61,17 +61,17 @@ const OurMission = () => {
       <Image
         src={Jagged}
         alt="Jagged Edge"
-        className="z-0 col-start-1 row-start-1 mt-130 hidden w-full md:block"
+        className="z-0 col-start-1 row-start-1 mt-130 hidden w-full md:mt-115 md:block"
       />
       <Image
         src={JaggedMobile}
         alt="Jagged Edge Mobile"
-        className="z-0 col-start-1 row-start-1 mt-150 block w-full md:hidden"
+        className="z-0 col-start-1 row-start-1 mt-150 block w-full scale-x-[1.01] md:hidden"
       />
       <Image
         src={Flower}
         alt="Green Flower"
-        className="z-20 -mt-92 -ml-38 hidden scale-40 md:-mt-115 md:-ml-30 md:scale-100 lg:block"
+        className="absolute right-[46vw] bottom-[3vh] z-20 scale-35 sm:bottom-[25vh] sm:-left-[25vw] sm:scale-45 md:-bottom-[4vh] md:-left-[20vw] lg:bottom-[5vh] lg:-left-[13vw] lg:scale-70 xl:bottom-[12vh] xl:scale-90 2xl:top-[65vh] 2xl:-left-[10vw] 2xl:scale-100"
       />
     </div>
   );
--- src/components/landing/Title.tsx
+++ src/components/landing/Title.tsx
@@ -7,18 +7,19 @@ const Title = () => {
   return (
     <div className="w-full">
       <Image
-        className="absolute -top-6 -left-3 -z-1 w-screen origin-top-left scale-200 md:-top-15 md:-top-[1.75%] md:-left-0 lg:-top-6 lg:scale-100 xl:-top-14"
+        className="absolute inset-0 -top-10 -left-3 -z-1 origin-top-left scale-y-[1.20] object-fill sm:scale-y-[1.50] md:-top-15 md:-left-0 md:scale-100 lg:-top-6 xl:-top-15 xl:scale-y-[1.3]"
         src={NotebookBackground}
         alt="Notebook Background"
+        fill
       />
       <div className="mx-auto w-full px-2">
         <div className="flex flex-col items-center justify-between text-center md:flex-row md:text-left">
           <div className="mt-[7%] flex w-4/6 flex-col md:-mt-50 md:ml-[10%] md:w-3/6">
-            <div className="font-enviro-single-day text-enviro-green-300 text-3xl md:text-4xl md:whitespace-nowrap lg:text-5xl xl:text-7xl">
+            <div className="font-enviro-single-day text-enviro-green-300 text-3xl sm:text-6xl md:text-5xl md:whitespace-nowrap lg:text-6xl xl:text-7xl">
               Environmental Science Club
             </div>
 
-            <div className="font-enviro-open-sans text-enviro-green-200 max-w-lg text-sm leading-relaxed font-semibold md:text-lg lg:text-2xl xl:text-3xl">
+            <div className="font-enviro-open-sans text-enviro-green-200 max-w-lg text-sm leading-relaxed font-semibold sm:text-2xl md:text-xl lg:text-2xl xl:text-3xl">
               Informing, empowering, and mobilizing students to lead
               sustainability efforts at UC Riverside.
             </div>
@@ -26,14 +27,14 @@ const Title = () => {
           <Image
             src={Logo}
             alt="Environmental Science Club @ UCR Logo"
-            className="w-3/5 object-contain drop-shadow-md md:-ml-[5%] md:w-full"
+            className="w-3/5 object-contain drop-shadow-md md:-ml-[15%] md:w-full md:scale-60 lg:-ml-[5%] lg:scale-80"
           />
         </div>
-        <div className="z-10 -mt-10 w-full scale-50 md:m-15 md:-mt-[15%] md:ml-[10%] md:w-1/4 md:scale-100">
+        <div className="z-10 -mt-10 w-full scale-50 md:absolute md:top-[62vh] md:left-[11vw] md:w-1/4 md:scale-100 lg:top-[65vh] lg:left-[10.5vw] lg:w-1/5 xl:top-[80vh] 2xl:top-[90vh]">
           <Button
-            href=""
+            href="/contact"
             children={"Contact Us"}
-            textSize="text-4xl md:text-2xl lg:text-3xl xl:text-4xl"
+            textSize="text-4xl md:text-2xl xl:text-4xl"
           />
         </div>
       </div>
