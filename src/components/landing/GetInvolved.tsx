8434525669a70f832c9a8ad0b2330b78efff4656
Howard Wheeler <167825355+Howardwheeler@users.noreply.github.com>
2026-04-02 01:56:13 -0700

    landing page responsify and redesign

--- src/components/landing/GetInvolved.tsx
+++ src/components/landing/GetInvolved.tsx
@@ -1,5 +1,90 @@
+import Subheading from "@/components/Subheading";
+import Image from "next/image";
+import GreenFlower from "@/public/Home/Greenflower.webp";
+import BlueFlower from "@/public/Home/BlueFlower.webp";
+import Hill from "@/public/Home/Hill.svg";
+import Grass from "@/public/Home/Grass.svg";
+import Link from "next/link";
+import footerData from "@/data/footer";
+
 const GetInvolved = () => {
-  return <div></div>;
+  const [flower0, flower1, flower2] = footerData;
+
+  return (
+    <div className="relative w-full">
+      <div className="bg-enviro-blue-100 absolute inset-0 -z-10 w-full opacity-31 -mt-[8%]" />
+      <div className="relative z-10 my-[8%] flex items-center justify-center text-white">
+        <Subheading title="Get Involved" bg="bg-enviro-green-100" text="" />
+      </div>
+
+      <div className="grid w-screen">
+        <div className="flex col-start-1 row-start-1 items-center justify-between gap-x-[3%] w-screen px-[5%]">
+          <div className="z-10 grid items-center justify-center">
+            <Image
+              src={GreenFlower}
+              alt="Flower"
+              className="col-start-1 row-start-1 translate-y-[4.7%] sm:translate-y-[3%] lg:translate-y-[5%]"
+            />
+            <div className="col-start-1 row-start-1 mb-[40%] flex items-center justify-center">
+              <Link href={flower0.link} target="_blank">
+                <flower0.Icon className="translate-y-0 text-2xl text-white sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl" />
+              </Link>
+            </div>
+          </div>
+
+          <div className="z-10 grid items-center justify-center">
+            <Image
+              src={BlueFlower}
+              alt="Flower"
+              className="col-start-1 row-start-1 translate-y-[0%] sm:-translate-y-[2%] lg:-translate-y-[0%]"
+            />
+            <div className="col-start-1 row-start-1 mb-[67%] flex justify-center text-center">
+              <Link href={flower1.link} target="_blank">
+                <flower1.Icon className="translate-y-0 text-2xl text-white sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl" />
+              </Link>
+            </div>
+          </div>
+
+          <div className="z-10 grid items-center justify-center ">
+            <Image
+              src={GreenFlower}
+              alt="Flower"
+              className="col-start-1 row-start-1 translate-y-[4.7%] lg:translate-y-[4.7%]"
+            />
+            <div className="col-start-1 row-start-1 mb-[40%] flex justify-center">
+              <Link href={flower2.link} target="_blank">
+                <flower2.Icon className="translate-y-0 text-2xl text-white sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl" />
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        
+        <div className="flex flex-row mt-auto z-10 w-full col-start-1 row-start-1">
+          <Image
+            src={Grass}
+            alt="Grass"
+            className="ml-[30%] mb-[5%] w-1/12"
+          />
+          <Image
+            src={Grass}
+            alt="Grass"
+            className=" ml-[18%] -mb-[8%] w-1/12"
+          />
+          <Image
+            src={Grass}
+            alt="Grass"
+            className="ml-[2%] mb-[12%] w-1/12"
+          />
+        </div>
+        <Image
+          src={Hill}
+          alt="Hill"
+          className="absolute bottom-0 z-0 w-full -mb-2"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default GetInvolved;
--- src/components/landing/Interested.tsx
+++ src/components/landing/Interested.tsx
@@ -4,65 +4,73 @@ import Join2 from "@/public/Home/Join2.webp";
 import Tape from "@/public/tape.webp";
 import Button from "@/components/Button";
 import SquigglyLine2 from "@/public/Home/SquigglyLine2.webp";
-import RippedPaperBlue from "@/public/RippedPaperBlue.webp";
+import RippedPaperBlue from "@/public/Home/RippedPaperBlue.webp";
+import RippedPaperBlueMobile from "@/public/Home/RippedPaperBlueMobile.webp";
 
 const Interested = () => {
   return (
-    <div className="relative grid">
+    <div className="relative grid w-full -mb-[5%]">
       <Image
         src={RippedPaperBlue}
         alt="Ripped Paper Background"
-        className="z-0 col-start-1 row-start-1 w-full"
+        className="z-0 col-start-1 row-start-1 -mb-5 hidden w-full md:block"
       />
-      <div className="z-10 col-start-1 row-start-1 mt-80 flex flex-row">
-        <div className="text-enviro-green-300 ml-10 flex w-4/10 flex-col gap-y-10">
+      <Image
+        src={RippedPaperBlueMobile}
+        alt="Ripped Paper Background"
+        className="z-0 col-start-1 row-start-1 block w-full md:hidden"
+      />
+      <div className="z-10 col-start-1 row-start-1 mx-auto mt-40 flex w-11/12 flex-col md:mx-0 md:mt-[22%] md:w-full md:flex-row">
+        <div className="text-enviro-green-300 flex w-full flex-col gap-y-[10%] md:ml-10 md:w-4/10">
           <div className="flex flex-col items-center">
-            <p className="font-enviro-single-day text-6xl">
+            <p className="font-enviro-single-day mt-3 text-3xl lg:text-5xl xl:text-6xl">
               Interested in joining?
             </p>
             <Image
               src={SquigglyLine2}
               alt="squiggly line"
-              className="w-auto pt-3"
+              className="w-auto pt-[3%]"
             />
           </div>
-          <p className="font-enviro-open-sans w-fit items-center text-3xl font-bold">
+          <p className="font-enviro-open-sans text-md w-fit items-center text-center font-bold md:text-left md:text-sm lg:text-2xl xl:text-3xl">
             Join our community of students passionate about sustainability! Get
             access to speaker events, volunteer opportunities, and hands-on
             climate resilience initiatives!
           </p>
-          <div className="w-2/3">
-            <Button href="" textSize="text-4xl">
+          <div className="mx-auto w-2/3 md:mx-0">
+            <Button href="" textSize="text-lg md:text-2xl lg:text-4xl xl:text-5xl">
               Join Here
             </Button>
           </div>
         </div>
-        <div className="absolute right-96 z-10 -rotate-3">
-          <div className="bg-enviro-gray-100 border-enviro-green-200 relative border-2 p-4 pb-20 shadow-md shadow-gray-500">
-            <Image
-              src={Tape}
-              alt="tape"
-              className="absolute -top-4 left-1/2 -translate-x-1/2 opacity-85"
-            />
-            <Image
-              src={Join1}
-              alt="club booth"
-              className="border-enviro-green-200 w-sm border-2"
-            />
+        <div className="my-auto flex w-full flex-row md:w-6/10">
+          <div className="z-20 w-full -rotate-3 pl-[3%]">
+            <div className="bg-enviro-gray-100 border-enviro-green-200 relative border-2 p-[4%] pb-[20%] shadow-md shadow-gray-500">
+              <Image
+                src={Tape}
+                alt="tape"
+                className="absolute -top-4 left-1/2 -translate-x-1/2 scale-50 opacity-85 md:scale-75 lg:scale-100"
+              />
+              <Image
+                src={Join1}
+                alt="club booth"
+                className="border-enviro-green-200 border-2"
+              />
+            </div>
           </div>
-        </div>
-        <div className="mt-20 ml-100 rotate-6">
-          <div className="bg-enviro-gray-100 border-enviro-green-200 relative border-2 p-4 pb-20 shadow-md shadow-gray-500">
-            <Image
-              src={Tape}
-              alt="tape"
-              className="absolute -top-4 left-1/2 -translate-x-1/2 opacity-85"
-            />
-            <Image
-              src={Join2}
-              alt="club booth 2"
-              className="border-enviro-green-200 w-sm border-2"
-            />
+          <div className="z-10 -ml-[5%] w-full rotate-8 pt-[6%] pr-[5%]">
+            <div className="bg-enviro-gray-100 border-enviro-green-200 relative border-2 p-[4%] pb-[20%] shadow-md shadow-gray-500">
+              <Image
+                src={Tape}
+                alt="tape"
+                className="absolute -top-4 left-1/2 -translate-x-1/2 scale-50 opacity-85 md:scale-75 lg:scale-100"
+              />
+              <Image
+                src={Join2}
+                alt="club booth 2"
+                className="border-enviro-green-200 border-2"
+              />
+            </div>
           </div>
         </div>
       </div>
